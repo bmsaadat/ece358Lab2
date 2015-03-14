82d7535a68e923cb9baf11e2679e9f71a5282c0e
Behroz Saadat <bmsaadat@uwaterloo.ca>
2015-03-14 17:32:42 -0400

    - Changed code to match proper ABP context

--- ABP.cpp
+++ ABP.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
+#include "ABP.h"
 
 using namespace std;
 
+void ABP::insert_event(EventType type, double time, bool errorFlag, int sequenceNumber) {
+    DiscreteEvent event;
+    event.setValues(type, time, errorFlag, sequenceNumber);
+    DES.push(event);
+}
+
+DiscreteEvent ABP::send() {
+    DiscreteEvent event = DES.top();
+    return event;
+}
+
+void ABP::simulate(double d) {
+    // The sequence number of the sending frame (SN)
+    sequenceNumber = 0;
+    // The sequence number of the next expected ACK
+    nextExpectedAck = 1;
+    // The sequence number of the next expected frame
+    nextExpectedFrame = 0;
+    
+    // Time-out value (delta)
+    delta = d;
+    // The current time (tc)
+    currentTime = 0;
+    // Length of frame header (H)
+    headerLength = 1000;
+    // Length of packet (l)
+    packetLength = 1000;
+    
+    
+    // Channel Capacity (C)
+    channelCapacity = 1000;
+    // Propagation delay (tau)
+    propagationDelay = 0;
+    // Bit Error Rate (BER)
+    bitErrorRate = 0;
+    
+    // Experiment Duration
+    numberOfPackets = 5000;
+
+    int totalPacketLength = headerLength + packetLength;
+    double transmissionDelay = (double)totalPacketLength / (double)channelCapacity;
+    
+    
+    // Start sending packets
+    for (int i = 0; i < numberOfPackets; i++) {
+        insert_event(TIMEOUT, currentTime + delta + transmissionDelay, false, sequenceNumber);
+        currentTime += transmissionDelay;
+        send();
+    }
+    
+    // Pseudo code
+    // Create TIMEOUT event at time tc+L/C+delta
+    // Call send()
+        //
+}
+
 int main() {
-	cout << "Hello World";
-	return 0;
+    ABP abpSimulator;
+    abpSimulator.simulate(10);
+    return 0;
 }
--- ABP.h
+++ ABP.h
@@ -12,17 +12,39 @@
 
 class ABP {
 protected:
-	int sn = 0;
-	int nextExpectedAck = 1;
+    // The sequence number of the sending frame (SN)
+	int sequenceNumber;
+    // The sequence number of the next expected ACK
+	int nextExpectedAck;
+    // The sequence number of the next expected frame
+    int nextExpectedFrame;
+    
+    // Time-out value (delta)
+    double delta;
+    // The current time (tc)
+    double currentTime;
+    // Length of frame header (H)
+    int headerLength;
+    // Length of packet (l)
+    int packetLength;
+    
+    
+    // Channel Capacity (C)
+    int channelCapacity;
+    // Propagation delay (tau)
+    double propagationDelay;
+    // Bit Error Rate (BER)
+    double bitErrorRate;
+    
+    // Experiment Duration
+    int numberOfPackets;
 
+    
 	// Priority queue of events, ordered by the time
 	std::priority_queue<DiscreteEvent, std::vector<DiscreteEvent>, LessThanByTime> DES;
-
 	void insert_event(EventType type, double time, bool errorFlag, int sequenceNumber);
-	void generate_arrival_packets();
-	void generate_observer_packets();
+    DiscreteEvent send();
 public:
-	void simulate(double row);
-	void simulator();
+	void simulate(double delta);
 };
 
